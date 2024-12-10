f3613e6ca9f0f52fac5381ea36b036a112f8d00a
Daniel <60996796+dbudiii@users.noreply.github.com>
2024-12-10 00:05:40 -0800

    added the API calling for BTC price.

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,12 +1,26 @@
-const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      'x-cg-demo-api-key': ''
-    }
-  };
-  
-  fetch('https://api.coingecko.com/api/v3/ping', options)
-    .then(res => res.json())
-    .then(res => console.log(res))
-    .catch(err => console.error(err));
+const apiKey = 'CG-syBgZNHgtsL74efX2XqVSZsR';
+
+async function fetchBTCPrice() {
+  try {
+    const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd', {
+      method: 'GET',
+      headers: {
+        'x-cg-api-key': apiKey
+      }
+    });
+
+    const data = await response.json();
+    console.log(data);
+    const price = data.bitcoin.usd;
+
+    document.getElementById('price').innerText = `Price: $${price.toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2})}`;
+  } 
+
+  catch (error) {
+    console.error('Error fetching data:', error);
+    document.getElementById('price').innerText = "Error loading price.";
+  }
+
+}
+
+fetchBTCPrice();
